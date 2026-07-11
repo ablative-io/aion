6baa9a168c5694052540423e4237299f5a7ece13
tomWhiting <tom.whiting@reachsocialsupports.com.au>
2026-07-11 21:14:43 +1000

    feat(editors): add AWL Zed extension

---
+++ editors/zed-awl/languages/awl/outline.scm
@@ -0,0 +1,6 @@
+(workflow_declaration name: (identifier) @name) @item
+(type_declaration name: (type_identifier) @name) @item
+(worker_declaration name: (identifier) @name) @item
+(action_declaration name: (identifier) @name) @item
+(child_declaration name: (identifier) @name) @item
+(step_declaration name: (identifier) @name) @item
